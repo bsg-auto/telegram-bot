8737765f50f9fd6cab845ab65cbf170cdfe7fb88
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2020-02-13 15:02:17 +0330

    "node-telegram-bot-api" was replaced with "telegraf".

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 /* global require, process */
 
 const http = require('http')
+const Telegraf = require('telegraf')
 
 const {
 	getExternalIP,
 } = require('./utils')
-
-const TelegramBot = require('node-telegram-bot-api')
 
 /**
  * Created on 1398/11/24 (2020/2/13).
@@ -15,7 +14,7 @@ const TelegramBot = require('node-telegram-bot-api')
 'use strict'
 const env = process.env
 
-getExternalIP().then(console.log.bind(console, 'Public IP:')).catch(console.error.bind(console))
+getExternalIP().then(console.log.bind(console,'Public IP:')).catch(console.error.bind(console))
 
 const PORT = env.PORT || 6000
 
@@ -24,32 +23,50 @@ http.createServer((req, res) => {
 	return res.end('Hello world!')
 }).listen(PORT)
 console.log(`Listening on port ${PORT} ...`)
+//***********************************************************************************************************/
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = env.TELEGRAM_API_KEY
+const bot = new Telegraf(env.TELEGRAM_API_KEY)
+bot.catch((err, ctx) => console.log(`Ooops! The bot encountered an error for ${ctx.updateType}`, err))
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, env.REMOTE_HOST ? {webHook: {host: env.REMOTE_HOST}} : {polling: true})
+// bot.use((ctx, next) => {
+// 	next()
+// 	console.log('ctx.telegram', ctx.telegram)
+// 	console.log('ctx.updateType', ctx.updateType)
+// 	console.log('ctx.updateSubTypes', ctx.updateSubTypes)
+// 	console.log('ctx.message', ctx.message)
+// 	console.log('ctx.editedMessage', ctx.editedMessage)
+// 	console.log('ctx.inlineQuery', ctx.inlineQuery)
+// 	console.log('ctx.chosenInlineResult', ctx.chosenInlineResult)
+// 	console.log('ctx.callbackQuery', ctx.callbackQuery)
+// 	console.log('ctx.shippingQuery', ctx.shippingQuery)
+// 	console.log('ctx.preCheckoutQuery', ctx.preCheckoutQuery)
+// 	console.log('ctx.channelPost', ctx.channelPost)
+// 	console.log('ctx.editedChannelPost', ctx.editedChannelPost)
+// 	console.log('ctx.poll', ctx.poll)
+// 	console.log('ctx.pollAnswer', ctx.pollAnswer)
+// 	console.log('ctx.chat', ctx.chat)
+// 	console.log('ctx.from', ctx.from)
+// 	console.log('ctx.match', ctx.match)
+// 	console.log('ctx.webhookReply', ctx.webhookReply)
+// })
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-	// 'msg' is the received Message from Telegram
-	// 'match' is the result of executing the regexp above on the text content
-	// of the message
-	
-	const chatId = msg.chat.id
-	const resp = match[1] // the captured "whatever"
-	
-	// send back the matched "whatever" to the chat
-	bot.sendMessage(chatId, resp)
+const welcomeMessage = `سلام✋
+به ربات #پاسخگویی_خودکار به پرسش‌های باشگاه کارگزاری آگاه خوش‌آمدید. 🌷
+اگر هنوز کد بورسی ندارید یا دارید ولی توی #آگاه نیستید، ما با توجه به مقایسه‌ای که بین کارگزاری‌های مختلف انجام داده‌ایم، این کارگزاری رو به عنوان #کارگزاری_برتر به شما پیشنهاد می‌کنیم.
+می‌تونید همین الان از طریق لینک زیر وارد بشید و ثبت‌نام اینترنتی‌تونو تکمیل کنید.
+توجه داشته باشید که این لینک، یک #لینک_معرفی هست و شما با استفاده از اون، علاوه بر #دریافت_امتیاز، تا یک ماه از امتیازات سطح معرف خود که شامل #۱۷درصد_تخفیف_کارمزد_معاملات و #مزایای_دیگه-ست بهره‌مند می‌شید!`
+bot.start(ctx => ctx.reply(welcomeMessage))
+bot.help(ctx => ctx.reply('Send me a sticker'))
+bot.on('sticker', ctx => ctx.reply('👍'))
+bot.hears('hi', ctx => ctx.reply('Hey there'))
+
+bot.use(async (ctx, next) => {
+	const start = new Date()
+	await next()
+	const ms = new Date() - start
+	console.log('Response time: %sms', ms)
 })
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', msg => {
-	console.log(msg)
-	const chatId = msg.chat.id
-	
-	// send a message to the chat acknowledging receipt of their message
-	bot.sendMessage(chatId, 'Received your message' + msg)
-})
+bot.on('text', (ctx) => ctx.reply('Hello World'))
+
+bot.launch()
