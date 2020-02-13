ff2bbf1f6a3f6ad83581f14546270b1a2f47353c
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2020-02-13 08:37:00 +0330

    `webHook: {host: env.REMOTE_HOST}` was added.

--- main.js
+++ main.js
@@ -17,11 +17,19 @@ const env = process.env
 
 getExternalIP().then(console.log.bind(console, 'Public IP:')).catch(console.error.bind(console))
 
+const PORT = env.PORT || 6000
+
+http.createServer((req, res) => {
+	console.log(req.url)
+	return res.end('Hello world!')
+}).listen(PORT)
+console.log(`Listening on port ${PORT} ...`)
+
 // replace the value below with the Telegram token you receive from @BotFather
 const token = env.TELEGRAM_API_KEY
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true})
+const bot = new TelegramBot(token, env.REMOTE_HOST ? {webHook: {host: env.REMOTE_HOST}} : {polling: true})
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -38,17 +46,10 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on('message', (msg) => {
+bot.on('message', msg => {
+	console.log(msg)
 	const chatId = msg.chat.id
 	
 	// send a message to the chat acknowledging receipt of their message
-	bot.sendMessage(chatId, 'Received your message')
+	bot.sendMessage(chatId, 'Received your message' + msg)
 })
-
-const PORT = env.PORT || 6000
-
-http.createServer((req, res) => {
-	console.log(req.url)
-	return res.end('Hello world!')
-}).listen(PORT)
-console.log(`Listening on port ${PORT} ...`)
